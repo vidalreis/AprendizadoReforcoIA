fd6ecf64b767e1d7001ef7fa3bba8381f23d2aa5
vidalreis <vidal.reis92@gmail.com>
2025-05-12 21:27:21 -0300

    build: feito até o quarto exercício

--- Atividades/AprenderNumPY.py
+++ Atividades/AprenderNumPY.py
@@ -1,2 +1,21 @@
-import numpy
+import numpy as np
 
+# 1. Criar um vetor nulo de tamanho 10.
+teste = [10]
+print("1 -", np.zeros(teste))
+
+# 2. Criar um vetor nulo de tamanho 10, mas o quarto item possui valor 1
+teste = np.zeros(10)
+teste[3] = 1
+print("2 -", teste)
+
+# 3. Criar um vetor com valores que variam de 10 a 49.
+teste = np.arange(9, 50) 
+print("3 -", teste)
+
+# 4. Criar uma matriz 3x3 com valores que variam de 0 a 9.
+#   a) Encontrar valor mínimo
+#   b) Encontrar valor máximo
+
+matriz = np.arange(9).reshape(3, 3)
+print("4 -\n", matriz)
