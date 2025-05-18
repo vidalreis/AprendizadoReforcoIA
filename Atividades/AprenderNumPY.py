b354cfa0ce934d24b244326251fcad4aba8ab205
vidalreis <vidal.reis92@gmail.com>
2025-05-18 01:36:20 -0300

    build exercício 6 finalizado

--- Atividades/AprenderNumPY.py
+++ Atividades/AprenderNumPY.py
@@ -19,8 +19,15 @@ print("3 -", teste)
 
 matriz = np.arange(9).reshape(3, 3)
 print("4 -\n", matriz)
-print("4 - Mínimo\n", np.min(matriz))
-print("4 - Mínimo\n", np.max(matriz))
+print("4 - a) Mínimo\n", np.min(matriz))
+print("4 - b) Máximo\n", np.max(matriz))
 
 # 5. - Criar uma matriz identidade 3x3.
 print("5 -\n", (np.eye(3,3)))
+
+# 6. - Subtrair a média de cada linha de uma matriz.
+matriz = np.arange(9).reshape(3, 3)
+media_linhas = matriz.mean(axis=1)
+print("6 - Matriz original:\n", matriz)
+print("6 - Média de cada linha:", media_linhas.flatten())
+
